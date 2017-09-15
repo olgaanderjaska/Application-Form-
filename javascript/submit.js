e1ead9bd16f68fb5d93876069abe06f1aa018a3b
Olga Anderjaska <olgaanderjaska@yahoo.com>
2017-09-15 12:08:06 -0400

    some updates

---
+++ javascript/submit.js
@@ -0,0 +1,14 @@
+ 
+
+$(document).ready(function(){
+    $("button").click(function(){
+        alert("Your application was successfully submited!");
+        console.log("button");
+        $("#subForm").reset();
+        console.log("subForm");
+       
+       
+    });
+  });
+
+// 
