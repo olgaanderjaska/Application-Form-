86e1a3e3adb73ea8febb2f01eadb3aa17f904102
Olga Anderjaska <olgaanderjaska@yahoo.com>
2017-11-01 16:18:57 -0400

    final

--- javascript/submit.js
+++ javascript/submit.js
@@ -1,14 +1,35 @@
  
 
-$(document).ready(function(){
-    $("button").click(function(){
-        alert("Your application was successfully submited!");
-        console.log("button");
-        $("#subForm").reset();
-        console.log("subForm");
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         alert("Your application was successfully submited!");
+//         console.log("button");
+//         $("#subForm").reset();
+//         console.log("subForm");
        
        
-    });
-  });
+//     });
+//   });
 
-// 
+var submitButton = document.getElementById("submitButton");
+console.log(submitButton);
+
+submitButton.addEventListener("click", function(){
+      alert("Yor application was successfully submited!");
+ })
+ var target = (" ");
+
+function subForm() {
+	return(" ");
+}
+$("#subForm").empty();
+console.log(" ");
+ // if (target.value == target.attr("subForm") {
+ //     target.value = " ";
+ // }
+ 
+ // })
+ // }	
+
+
+
